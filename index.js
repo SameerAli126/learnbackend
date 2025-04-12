838b3d11a09d45e2dd46b95b21cd86143c9808af
SAM <ytytyt456789@gmail.com>
2025-04-12 14:33:23 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/login', (req, res) => {
+    res.send(`
+        <h1>Kindly login at 
+            <a href="https://opti-trade-2-0-87y5efwoj-sameerali126s-projects.vercel.app/" 
+               target="_blank" 
+               style="color: blue; text-decoration: underline;">
+                OptiTrade
+            </a>
+        </h1>
+    `);
+});
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+})
